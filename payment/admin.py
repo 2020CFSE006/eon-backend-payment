eb38b049b92afdaf19a8d6a07af10c089bea2f81
mohit.manuja <mohit.manuja@hashedin.com>
2020-04-22 13:18:47 +0530

    First Commit

---
+++ payment/admin.py
@@ -0,0 +1,33 @@
+"""
+ Register your models here.
+"""
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+
+# Register your models here.
+from payment.models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    """
+    Method to show filds for admin
+    """
+    list_display = ("id", 'amount', 'discount_amount', 'total_amount', 'status', 'ref_number')
+    list_filter = ("status",)
+    search_fields = ('id', 'status', 'ref_number')
+    readonly_fields = ('amount', 'discount_amount', 'total_amount', 'status', 'ref_number')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+admin.site.unregister(Group)
+
+admin.site.unregister(User)
