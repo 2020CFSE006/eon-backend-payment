c0c15a06fd7c5350a47cfc65ed9707583dd347e1
mohit.manuja <mohit.manuja@hashedin.com>
2020-05-02 01:03:27 +0530

    placed previous encoded key back

--- payment/auth.py
+++ payment/auth.py
@@ -1,7 +1,7 @@
 import jwt
 from rest_framework import authentication
 from rest_framework import exceptions
-from eon_payment.settings import DECODE_KEY, NEW_DECODE_KEY
+from eon_payment.settings import DECODE_KEY
 
 
 class CustomJWTAuthentication(authentication.BaseAuthentication):
@@ -15,7 +15,7 @@ class CustomJWTAuthentication(authentication.BaseAuthentication):
         if token_length > 2 or token_length < 2 or token[0] != 'Bearer':
             raise exceptions.AuthenticationFailed
         try:
-            payload = jwt.decode(token[1], NEW_DECODE_KEY, algorithms=['HS256'])
+            payload = jwt.decode(token[1], DECODE_KEY, algorithms=['HS256'])
         except:
             raise exceptions.ValidationError({'message': 'Signature Validation Failed'})
 
